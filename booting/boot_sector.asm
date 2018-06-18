d90b218cf7913f94c0c5760e7100404bbf0129a7
Sanjana Sunil <sanjana99sunil@gmail.com>
2018-06-18 10:41:06 +0530

    Added details of language

--- booting/boot_sector.asm
+++ booting/boot_sector.asm
@@ -1,6 +1,14 @@
+; NASM
+ 
 loop:                     ; Define a 'loop' label 
        jmp loop              
 
-times 510-($-$$) db 0     ; Continue filling with zeroes (db 0) until the 510th byte   
+times 510-($-$$) db 0     ; Continue filling with zeroes (db 0) until the 510th byte
+			  ; $ refers to current address of statement 
+			  ; $$ refers to address of start of section
+		          ; ($-$$) is the current position of section
+			  ; 510-($-$$) fills with 510 minus size of previous code
+			  ; db 0 is define byte 0
+	                  ; times repeats the instruction  
 
 dw 0xaa55                 ; Remaining 2 bytes is 0xaa55 indicating it is a boot sector to BIOS
